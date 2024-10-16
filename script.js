ce5b567a9693121932249a1774b5bfd5d85614bd
Ritu Bafna <ritujane78@gmail.com>
2024-10-16 21:13:45 +0100

    change css

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
 
-  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('body').style.backgroundColor = '#440d0d';
   document.querySelector('.number').style.width = '15rem';
 });
 
@@ -104,7 +104,7 @@ Implement a game rest functionality, so that the player can make a new guess! He
 1. Select the element with the 'again' class and attach a click event handler
 2. In the handler function, restore initial values of the score and secretNumber variables
 3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
+4. Also restore the original background color (#440d0d) and number width (15rem)
 
 GOOD LUCK 😀
 */
